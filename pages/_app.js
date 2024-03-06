36570c10d57238c1b3a2668600462af4d60c784d
provishalk <72138651+provishalk@users.noreply.github.com>
2024-03-06 11:51:07 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,18 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <main>
+      <script dangerouslySetInnerHTML={{__html:`(function (w, d, s, o, f, js, fjs) { w[o] = w[o] || function () { (w[o].q =
+          w[o].q || []).push(arguments); }; (js = d.createElement(s)), (fjs =
+          d.getElementsByTagName(s)[0]); js.id = o; js.src = f; js.async = 1;
+          fjs.parentNode.insertBefore(js, fjs); })(window, document, "script", "_cvt",
+          "https://cdn.convrrt.com/libs/popup/widget.js");
+          _cvt("init", {
+            orgID: 'mailmodo',
+            projectID: "0004ede9-b0b1-55d5-b0b2-819243f1c0ea_f5ad2069-61cf-4503-8b70-7e7b6eae5812",
+            popupID: "749b058c-264a-43fe-abe0-d748ce781089"
+          });`}}>
+        {}
+      </script>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
